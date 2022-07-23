b3831e02829818a4797ded9722d0ca8ed89b98db
Vishnu Prasad Preetham Kumar <81408708+Wish-nu@users.noreply.github.com>
2022-07-23 21:18:38 +0530

    Create KarakasiaSakshamAssign3ApplicationTests.java

---
+++ KarakasiaSakshamAssign3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.karakasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarakasiaSakshamAssign3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
